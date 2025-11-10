013e55dfdfd59b80a0fbf834d9cfeff47394d748
Agnese <165146408+AgneseBeschin@users.noreply.github.com>
2025-11-10 23:48:48 +0100

    Update DB.sql
    Ho modificato il check per la lunghezza dell'indirizzo email: invece di 20 caratteri ho messo max 100, altrimenti era troppo poco
    
    Ho aggiunto il check che un follower non può seguire se stesso

--- Database/DB.sql
+++ Database/DB.sql
@@ -1,30 +1,109 @@
 use progettotec;
 
-DROP TABLE IF EXISTS Utente;
+DROP TABLE IF EXISTS LikePost;
+DROP TABLE IF EXISTS Commento;
+DROP TABLE IF EXISTS Post;
+DROP TABLE IF EXISTS Foto;
+DROP TABLE IF EXISTS Ban;
 DROP TABLE IF EXISTS UAdmin;
+DROP TABLE IF EXISTS Utente;
 
-CREATE TABLE Utente
-(
-	Email varchar(100) PRIMARY KEY,
-    Username varchar(20),
-    Pass char(64),
-    Stato enum("Blocked","Non Blocked"),
-    Livello enum("Cat Apprentice","Cat Whisperer","Pat Wizard","Pat Master"),
-    Punti smallint UNSIGNED,
-    constraint Stato CHECK(Stato in ("Blocked","Non Blocked")),
-    constraint Email CHECK(LENGTH(Email) > 7 AND LENGTH(Email) <= 20),
-    constraint Username CHECK(LENGTH(Username) > 0 AND LENGTH(Username) <= 20),
-	constraint Pass CHECK(LENGTH(Pass) = 64)
+CREATE TABLE Utente (
+    Email VARCHAR(100) PRIMARY KEY,
+    Username VARCHAR(20) UNIQUE NOT NULL,
+    Pass CHAR(64) NOT NULL,
+    Stato ENUM('Blocked', 'Non Blocked') DEFAULT 'Non Blocked',
+    Livello ENUM('Cat Apprentice', 'Cat Whisperer', 'Pat Wizard', 'Pat Master') DEFAULT 'Cat Apprentice',
+    Punti SMALLINT UNSIGNED DEFAULT 0,
+    CONSTRAINT ck_email CHECK (LENGTH(Email) > 7 AND LENGTH(Email) <= 100),
+    CONSTRAINT ck_username CHECK (LENGTH(Username) > 0 AND LENGTH(Username) <= 20),
+    CONSTRAINT ck_pass CHECK (LENGTH(Pass) = 64)
 );
 
-CREATE TABLE UAdmin
-(
-	EmailA varchar(100) PRIMARY KEY,
-    UsernameA varchar(20),
-    PassA char(64),
-	constraint EmailA CHECK(LENGTH(EmailA) > 7 AND LENGTH(EmailA) <= 20),
-    constraint UsernameA CHECK(LENGTH(UsernameA) > 5 AND LENGTH(UsernameA) <= 20),
-	constraint PassA CHECK(LENGTH(PassA) = 64)
+CREATE TABLE UAdmin (
+    Email VARCHAR(100) PRIMARY KEY,
+    Username VARCHAR(20) UNIQUE NOT NULL,
+    Pass CHAR(64) NOT NULL,
+    CONSTRAINT ck_admin_email CHECK (LENGTH(Email) > 7 AND LENGTH(Email) <= 100),
+    CONSTRAINT ck_admin_username CHECK (LENGTH(Username) > 0 AND LENGTH(Username) <= 20),
+    CONSTRAINT ck_admin_pass CHECK (LENGTH(Pass) = 64)
+);
+
+CREATE TABLE Foto (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Percorso VARCHAR(255) NOT NULL,
+
+    -- Dati geografici:
+    Latitudine DECIMAL(10, 8),
+    Longitudine DECIMAL(11, 8),
+
+    ColoreGatto ENUM('Nero', 'Bianco', 'Rosso/Arancio', 'Grigio', 'Tigrato', 'Tartaruga', 'Multicolore'),
+    EtaGatto ENUM ('Cucciolo', 'Adulto', 'Anziano'),
+    DescrizioneBreve VARCHAR(255)
+);
+
+-- Se banniamo un utente i suoi post restano nel db, ma EmailAutore diventa NULL
+CREATE TABLE Post (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    EmailAutore VARCHAR(100),
+    FotoID INT NOT NULL,
+    Testo TEXT,
+    DataOra DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (EmailAutore) REFERENCES Utente(Email)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (FotoID) REFERENCES Foto(ID)
+        ON DELETE RESTRICT -- Una foto può essere riusata, ma non cancellata se in uso
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE Commento (
+    Email VARCHAR(100),
+    PostID INT,
+    DataOra DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Testo TEXT NOT NULL,
+    PRIMARY KEY (Email, PostID, DataOra),
+    FOREIGN KEY (Email) REFERENCES Utente(Email)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (PostID) REFERENCES Post(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- RELAZIONE BAN (Admin -<>- Utente)
+CREATE TABLE Ban (
+    EmailAdmin VARCHAR(100),
+    EmailUtente VARCHAR(100),
+    Motivo TEXT NOT NULL,
+    DataOra DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (EmailAdmin, EmailUtente),
+    FOREIGN KEY (EmailAdmin) REFERENCES UAdmin(Email)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (EmailUtente) REFERENCES Utente(Email)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- RELAZIONE LIKE (Utente -<>- Post)
+CREATE TABLE LikePost (
+    Email VARCHAR(100),
+    PostID INT,
+    PRIMARY KEY (Email, PostID),
+    FOREIGN KEY (Email) REFERENCES Utente(Email)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (PostID) REFERENCES Post(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- RELAZIONE FOLLOW (Utente -<>- Utente)
+CREATE TABLE Follow (
+    Follower VARCHAR(100),
+    Followed VARCHAR(100),
+    PRIMARY KEY (Follower, Followed),
+    FOREIGN KEY (Follower) REFERENCES Utente(Email)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Followed) REFERENCES Utente(Email)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    -- un utente non può seguire se stesso:
+    CONSTRAINT ck_no_self_follow CHECK (Follower <> Followed)
 );
 
 /*dati provvisori, non rispettano i controlli (che sono ancora da fare lato JS e PHP) : test123*/
